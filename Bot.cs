79ac5e97e59420e74387de244bb60003efc7f57a
im4i4a <al.zemskov03@yandex.ru>
2025-11-10 22:39:18 +0300

    Добавил функционал постоянного сервиса

--- Bot.cs
+++ Bot.cs
@@ -8,16 +8,63 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.Hosting;
 
 namespace VoiceTexterBot
 {
-    class Bot
+    class Bot: BackgroundService
     {
         private ITelegramBotClient _telegramClient;
 
         public Bot(ITelegramBotClient telegramClient)
         {
             _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions() { AllowedUpdates = { } }, // Здесь выбираем, какие обновления хотим получать. В данном случае разрешены все
+                cancellationToken: stoppingToken);
+
+            Console.WriteLine("Бот запущен");
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, 
+            Update update, 
+            CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramClient.SendMessage(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
+                return;
+            }
+
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegramClient.SendMessage(update.Message.Chat.Id, "Вы отправили сообщение", cancellationToken: cancellationToken);
+                return;
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, 
+            Exception exception, 
+            CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(errorMessage);
+
+            Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
         }
     }
 }
