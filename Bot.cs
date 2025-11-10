bd6d022b44711af42ae7c59e9c67c64086f15872
im4i4a <al.zemskov03@yandex.ru>
2025-11-10 22:07:34 +0300

    Добавьте файлы проекта.

---
+++ Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace VoiceTexterBot
+{
+    class Bot
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VoiceTexterBot
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
